45cfb96343ed2ab00e9fdad7959022ad2a226fe6
kiara <kiaritapino11@gmail.com>
2025-04-14 21:56:37 -0400

    login

--- ProyectoIoT/Form1.Designer.cs
+++ ProyectoIoT/Form1.Designer.cs
@@ -28,13 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.boton_acceder = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(476, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(409, 591);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(24, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 44);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Iniciar Sesión";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 353);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Contraseña";
+            // 
+            // txtuser
+            // 
+            this.txtuser.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtuser.Location = new System.Drawing.Point(32, 279);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(352, 22);
+            this.txtuser.TabIndex = 4;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(32, 387);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(352, 22);
+            this.txtpass.TabIndex = 5;
+            this.txtpass.UseSystemPasswordChar = true;
+            // 
+            // boton_acceder
+            // 
+            this.boton_acceder.Location = new System.Drawing.Point(32, 481);
+            this.boton_acceder.Name = "boton_acceder";
+            this.boton_acceder.Size = new System.Drawing.Size(352, 35);
+            this.boton_acceder.TabIndex = 6;
+            this.boton_acceder.Text = "Acceder";
+            this.boton_acceder.UseVisualStyleBackColor = true;
+            this.boton_acceder.Click += new System.EventHandler(this.boton_acceder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(877, 589);
+            this.Controls.Add(this.boton_acceder);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Button boton_acceder;
     }
 }
 
--- ProyectoIoT/Form1.cs
+++ ProyectoIoT/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ProyectoIoT
 {
@@ -15,6 +10,103 @@ namespace ProyectoIoT
         public Form1()
         {
             InitializeComponent();
+
+            
+            txtuser.Text = "Usuario";
+            txtuser.ForeColor = Color.Green;
+
+            txtpass.Text = "Contraseña";
+            txtpass.ForeColor = Color.Green;
+            txtpass.UseSystemPasswordChar = false;
+
+            
+            txtuser.Enter += txtuser_Enter;
+            txtuser.Leave += txtuser_Leave;
+            txtpass.Enter += txtpass_Enter;
+            txtpass.Leave += txtpass_Leave;
+        }
+
+        private void boton_acceder_Click(object sender, EventArgs e)
+        {
+            string usuario = txtuser.Text;
+            string contra = txtpass.Text;
+
+            if (usuario == "Usuario" || contra == "Contraseña")
+            {
+                MessageBox.Show("Por favor, ingrese sus credenciales.", "Campos vacíos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            MySqlConnection conexionBD = conectar.conex();
+
+            try
+            {
+                MySqlCommand comando = new MySqlCommand("SELECT * FROM usuario WHERE usu = @usuario AND password = @contra", conexionBD);
+                comando.Parameters.AddWithValue("@usuario", usuario);
+                comando.Parameters.AddWithValue("@contra", contra);
+
+                MySqlDataReader reader = comando.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    reader.Close();
+                    Inicio formularioInicio = new Inicio();
+                    formularioInicio.FormClosed += (s, args) => this.Show(); 
+                    formularioInicio.Show();
+                    this.Hide(); 
+                }
+                else
+                {
+                    MessageBox.Show("Usuario o contraseña incorrectos.", "Error de inicio de sesión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar: " + ex.Message);
+            }
+            finally
+            {
+                conexionBD.Close();
+            }
+        }
+
+        private void txtuser_Enter(object sender, EventArgs e)
+        {
+            if (txtuser.Text == "Usuario")
+            {
+                txtuser.Text = "";
+                txtuser.ForeColor = Color.Green;
+            }
+        }
+
+        private void txtuser_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtuser.Text))
+            {
+                txtuser.Text = "Usuario";
+                txtuser.ForeColor = Color.Green;
+            }
+        }
+
+        private void txtpass_Enter(object sender, EventArgs e)
+        {
+            if (txtpass.Text == "Contraseña")
+            {
+                txtpass.Text = "";
+                txtpass.ForeColor = Color.Green;
+                txtpass.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtpass_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtpass.Text))
+            {
+                txtpass.Text = "Contraseña";
+                txtpass.ForeColor = Color.Green;
+                txtpass.UseSystemPasswordChar = false;
+            }
         }
     }
 }
